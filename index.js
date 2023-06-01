57fa49b11bc38d2df22b9d05eb0dfc24b91d0215
dev-rahim <rahimuddin5915@gmail.com>
2023-06-01 12:19:06 +0600

    user role update api done

--- index.js
+++ index.js
@@ -48,6 +48,19 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: `admin`,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      console.log(result);
+      res.send(result);
+    });
     // reviews related apis
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
